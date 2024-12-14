c8c0de8fd8bb07e220b7584fc635dd6f8c5b56ff
dobell733 <dominicbell30@outlook.com>
2024-12-14 10:37:46 -0700

    update table generation

--- server/database/initial_data.sql
+++ server/database/initial_data.sql
@@ -1,25 +1,25 @@
 -- Create Kids table
 CREATE TABLE Kids (
-  kid_id INT IDENTITY(1,1) PRIMARY KEY,
-  name NVARCHAR(50),
-  age INT,
-  points INT
+  kid_id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  age INTEGER,
+  points INTEGER
 );
 
 -- Create Chores table
 CREATE TABLE Chores (
-  chore_id INT IDENTITY(1,1) PRIMARY KEY,
-  chore_name NVARCHAR(60),
-  point_value INT
+  chore_id SERIAL PRIMARY KEY,
+  chore_name VARCHAR(60),
+  point_value INTEGER
 );
 
 -- Create Kids_Chores table (Intersection Table)
 CREATE TABLE Kids_Chores (
-  kids_chore_id INT IDENTITY(1,1) PRIMARY KEY,
-  kid_id INT FOREIGN KEY REFERENCES Kids(kid_id),
-  chore_id INT FOREIGN KEY REFERENCES Chores(chore_id),
-  is_locked BIT DEFAULT 0,
-  unlock_time DATETIME
+  kids_chore_id SERIAL PRIMARY KEY,
+  kid_id INTEGER REFERENCES Kids(kid_id),
+  chore_id INTEGER REFERENCES Chores(chore_id),
+  is_locked BOOLEAN DEFAULT false,
+  unlock_time TIMESTAMP
 );
 
 -- Insert data into Kids table
@@ -45,7 +45,7 @@ INSERT INTO Chores (chore_name, point_value) VALUES
 
 -- Assign chores to Reagan
 INSERT INTO Kids_Chores (kid_id, chore_id, is_locked, unlock_time)
-SELECT 3, chore_id, 0, NULL FROM Chores 
+SELECT 3, chore_id, false, NULL FROM Chores 
 WHERE chore_name IN (
   'Put Away Toys',
   'Make Your Bed',
@@ -59,8 +59,11 @@ WHERE chore_name IN (
 
 -- Assign chores to Sawyer and Brooke
 INSERT INTO Kids_Chores (kid_id, chore_id, is_locked, unlock_time)
-SELECT kid_id, chore_id, 0, NULL FROM Kids, Chores 
-WHERE name IN ('Sawyer', 'Brooke') AND chore_name IN (
+SELECT K.kid_id, C.chore_id, false, NULL 
+FROM Kids K 
+CROSS JOIN Chores C
+WHERE K.name IN ('Sawyer', 'Brooke') 
+AND C.chore_name IN (
   'Put Away Toys / Clean Up Clutter',  -- Changed for Sawyer and Brooke
   'Fold and Put Away Clothes',
   'Wash Your Dishes',
